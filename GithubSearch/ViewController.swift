007ce1801f76c294b90ef4e58e315ef3db7680ac
Abraham Tesfamariam <abe973t@gmail.com>
2020-04-01 17:09:58 -0400

    Initial Commit

---
+++ GithubSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubSearch
+//
+//  Created by mcs on 4/1/20.
+//  Copyright © 2020 MCS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
